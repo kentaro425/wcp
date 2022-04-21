726092f2cd259cf244a9576260c71c9a9f2f5491
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-04-21 21:34:59 +0900

    [finish]4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		int num = 6;
+		String str = "健汰郎";
+		System.out.println(num);
+		System.out.println(str);
+		str = "こんにちは";
+		System.out.println(str);
+	}
+}
