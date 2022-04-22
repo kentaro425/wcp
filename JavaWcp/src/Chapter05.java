e13b4394bc4a76071f11582d7d8180788a991bf5
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-04-22 23:56:12 +0900

    [finish]5

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,22 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 123242332322532L;
+		double double1 = 122.11;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		
+		Integer integer1 = 241;
+        System.out.println(integer1);
+        Float float1 = 3.255325f;
+        System.out.println(float1);
+        Character char1 = 'あ';
+        System.out.println(char1);
+		
+		int[] array1 = { 10, 20, 30, 40, 50};
+		System.out.println(array1[3]);
+	}
+
+}
