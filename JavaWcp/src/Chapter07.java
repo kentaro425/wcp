aee716782e5b2f79fe4a219ae8d1d1bfc0f2d559
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-04-26 18:07:00 +0900

    [finish]8

--- JavaWcp/src/Chapter07.java
+++ JavaWcp/src/Chapter07.java
@@ -11,22 +11,21 @@ public class Chapter07 {
 		} else if (distance > 15) {
 			System.out.println("とても遠いです");
 		}
-		
+
 		String color = "yellow"; // 例としてyellowで初期化。他の色でも良い
-        switch(color) {
-          case "red" :
-            System.out.println("赤信号です");
-            break;
-          case "yellow" :
-            System.out.println("黄信号です");
-            break;
-          case "blue" :
-            System.out.println("青信号です");
-            break;
-          default:
-            System.out.println("信号の色ではありません");
-        }
+		switch (color) {
+		case "red":
+			System.out.println("赤信号です");
+			break;
+		case "yellow":
+			System.out.println("黄信号です");
+			break;
+		case "blue":
+			System.out.println("青信号です");
+			break;
+		default:
+			System.out.println("信号の色ではありません");
+		}
 	}
-	
-	
+
 }
---
+++ JavaWcp/src/Chapter08.java
@@ -0,0 +1,15 @@
+
+public class Chapter08 {
+	public static void main(String[] args) {
+		int number = 1;
+		while (number < 5) {
+			System.out.println(number * number);
+			number ++;
+		}
+		
+		int array[] = { 1, 4, 14, 25 };
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+	}
+}
